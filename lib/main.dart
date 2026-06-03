0e357fe86252e654626687eb3494243803e44302
Khalid Anan Mohammed Tanboura <khalidtanboura@gmail.com>
2026-06-03 13:53:19 +0300

    amber_themed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.green)),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.amber)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
